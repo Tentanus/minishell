c910bbc8be71f5355a7ab917cc4bac9641635c48
Mares Verbrugge <maressirou@gmail.com>
2023-02-22 12:48:13 +0100

    start of linked list rewrite

--- src/builtin/init_env_var.c
+++ src/builtin/init_env_var.c
@@ -5,10 +5,16 @@
 ** it can be used everytime a new shell or child process is initiated.
 */
 
+int	add_var_to_end_list()
+{
+	
+	return (0);
+}
+
 t_env_var_ll	*init_new_var(char *env_var)
 {
 	t_env_var_ll 	*new_var;
-	int	i;
+	int				i;
 
 	new_var = (t_env_var_ll *)malloc(sizeof(t_env_var_ll));
 	if (!new_var)
@@ -19,15 +25,15 @@ t_env_var_ll	*init_new_var(char *env_var)
 		if (env_var[i] == '=')
 		{
 			new_var->name = ft_substr(env_var, 0, i);
-			i++;
-			new_var->value = ft_substr(env_var, i, ft_strlen(env_var));
+			new_var->value = ft_substr(env_var, i + 1, ft_strlen(env_var));
+			break ;
 		}
 		i++;
 	}
-	if (new_var->value != NULL)
-		new_var->has_value = true;
-	else
+	if (new_var->value == NULL)
 		new_var->has_value = false;
+	else
+		new_var->has_value = true;
 	new_var->next = NULL;
 	return (new_var);
 }
@@ -38,9 +44,10 @@ int	add_variables(char *env_var)
 
 	new_var = init_new_var(env_var);
 	if (!new_var)
-		return (minishell_error("fail in add_variables"), NULL);
+		return (minishell_error("fail in add_variables"), 1);
 	// positie van var in list?
-	// add_var_to_end_of_list? of add_var_to_list();
+	add_var_to_end_list(new_var);
+	// of add_var_to_list();
 	return (0);
 }
 
@@ -51,7 +58,8 @@ int	init_env_var(char **envp)
 	i = 0;
 	while (envp[i] != NULL)
 	{
-		add_variable(envp[i]);
+		if (add_variable(envp[i]) == 1);
+			return (minishell_error("fail in init_env_var"), 1);
 		i++;
 	}
 	return (0);
