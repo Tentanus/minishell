b7c5c563bb216306c1fea05672404d43afd18e3d
Martijn Weverling <mweverli@student.codam.nl>
2023-04-07 17:54:52 +0200

    HERE_DOC (v0.3) here_doc.c: added a short expander to the here_doc and minor corrections of mistakes ; still needs to be tested. will do that this weekend

--- src/executor/here_doc.c
+++ src/executor/here_doc.c
@@ -1,5 +1,22 @@
 #include <minishell.h>
 
+char	*here_replace_line(const char *line, const char env_value, \
+		size_t *pos, size_t env_key_len)
+{
+	char			new_line;
+	const size_t	env_value_len = ft_strlen(env_value);
+
+	new_line = malloc(sizeof(char) * (ft_strlen(line) - env_key_len + \
+				env_value_len + 1));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(new_line, line, *pos + 1);
+	ft_strlcat(new_line, env_value, env_value_len + 1);
+	ft_strcpy(new_line, line + *pos + env_value_len, \
+			ft_strlen(line + pos + env_key_len));
+	return (new_line);
+}
+
 char	*here_expand(const char *line, t_env_var_ll *list_env)
 {
 	char	*ret;
@@ -19,11 +36,14 @@ char	*here_expand(const char *line, t_env_var_ll *list_env)
 			if (!env_str)
 				return (NULL);
 			env_value = env_var_get_env(env_name, list_env);
-			line = here_replace_line(line, &i);
+			line = here_replace_line(line, env_value,  &i, var_len);
+			if (!line)
+				return (NULL);
 		}
 		i++;
 	}
 }
+
 void	here_init(const char delim, t_env_var_ll *list_env)
 {
 	int		pipe_fd[2];
@@ -37,13 +57,13 @@ void	here_init(const char delim, t_env_var_ll *list_env)
 		if (line == NULL)
 			return (-1);
 		line = here_expand(line, list_env);
-		write(pipe_fd[1], line, ft_strlen(line));
-		if (!ft_strncmp(line, delim, ft_strlen(delim)))
+		if (!ft_strncmp(line, delim, ft_strlen(delim) + 1))
 			break ;
+		write(pipe_fd[1], line, ft_strlen(line));
 		free(line);
 	}
 	close(pipe_fd[1]);
-	return (pipe_fd[o]);
+	return (pipe_fd[0]);
 }
 
 void	handle_here(t_cmd *cmd_list, t_env_var_ll *list_env)
