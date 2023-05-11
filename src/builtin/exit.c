b245c9224d8d052b1471eda70a23c9997a12f9ad
Mares Verbrugge <maressirou@gmail.com>
2023-05-11 15:16:34 +0200

    clear history removed from exit if not exit-ing

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: mverbrug <mverbrug@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/09 10:27:04 by mverbrug      #+#    #+#                 */
-/*   Updated: 2023/05/11 14:44:45 by mverbrug      ########   odam.nl         */
+/*   Updated: 2023/05/11 15:04:00 by mverbrug      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ int	builtin_exit(t_cmd *cmd)
 			if (ft_stris(cmd->args[1], ft_isdigit) == 1)
 				return (clear_history(), exit(ft_atoi(cmd->args[1])), SUCCESS);
 			if (ft_stris(cmd->args[1], ft_isdigit) == 0)
-				return (clear_history(), mini_error(error_print, \
+				return (mini_error(error_print, \
 					1, "exit: numeric argument required"), exit(255), ERROR);
 		}
 		if (ft_stris(cmd->args[1], ft_isdigit) == 1)
 			return (mini_error(error_print, 1, \
 				"exit: too many arguments"), ERROR);
 		if (ft_stris(cmd->args[1], ft_isdigit) == 0)
-			return (clear_history(), mini_error(error_print, \
+			return (mini_error(error_print, \
 				1, "exit: numeric argument required"), exit(255), ERROR);
 	}
 	clear_history();
@@ -50,14 +50,14 @@ int	builtin_exit_child(t_cmd *cmd)
 			if (ft_stris(cmd->args[1], ft_isdigit) == 1)
 				return (clear_history(), _exit(ft_atoi(cmd->args[1])), SUCCESS);
 			if (ft_stris(cmd->args[1], ft_isdigit) == 0)
-				return (clear_history(), mini_error(error_print, \
+				return (mini_error(error_print, \
 					1, "exit: numeric argument required"), _exit(255), ERROR);
 		}
 		if (ft_stris(cmd->args[1], ft_isdigit) == 1)
 			return (mini_error(error_print, 1, \
 				"exit: too many arguments"), ERROR);
 		if (ft_stris(cmd->args[1], ft_isdigit) == 0)
-			return (clear_history(), mini_error(error_print, \
+			return (mini_error(error_print, \
 				1, "exit: numeric argument required"), _exit(255), ERROR);
 	}
 	clear_history();
